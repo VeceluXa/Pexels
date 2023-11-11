d9af9ae5b32250fc72a8a14a31db24109791ed1d
Fyodor Danilov <danilovfa@protonmail.com>
2023-11-11 21:04:09 +0300

    Add bottom navigation indication animation

--- app/src/main/java/com/danilovfa/pexels/presentation/common/view/bottomnavigation/BottomBar.kt
+++ app/src/main/java/com/danilovfa/pexels/presentation/common/view/bottomnavigation/BottomBar.kt
@@ -1,12 +1,30 @@
 package com.danilovfa.pexels.presentation.common.view.bottomnavigation
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.danilovfa.pexels.presentation.common.animation.IconAnimatedVisibility
 import com.danilovfa.pexels.presentation.screen.NavGraphs
 import com.danilovfa.pexels.presentation.screen.appCurrentDestinationAsState
 import com.danilovfa.pexels.presentation.screen.destinations.Destination
@@ -21,19 +39,69 @@ fun BottomBar(
     val currentDestination: Destination = navController.appCurrentDestinationAsState().value
         ?: NavGraphs.root.startAppDestination
 
+    val isVisible = navigationTabs.map { it.direction as Destination }.contains(currentDestination)
+
     BottomNavigationBar(
         containerColor = MaterialTheme.colorScheme.background,
         contentColor = Color.Transparent,
-        isVisible = navigationTabs.map { it.direction as Destination }.contains(currentDestination)
+        isVisible = isVisible
     ) {
-        navigationTabs.forEach { navigationTab ->
-            BottomNavigationTab(
-                navController = navController,
-                tab = navigationTab,
-                destinations = navigationTabs,
-                selected = currentDestination == navigationTab.direction,
-                modifier = Modifier.fillMaxHeight()
-            )
+        Column {
+            Row(Modifier.fillMaxWidth()) {
+                ActiveTabIndication(
+                    position = navigationTabs.indexOfFirst { it.direction == currentDestination },
+                    tabsCount = navigationTabs.size,
+                    isVisible = isVisible
+                )
+            }
+            Row(Modifier.fillMaxWidth()) {
+                navigationTabs.forEach { navigationTab ->
+                    BottomNavigationTab(
+                        navController = navController,
+                        tab = navigationTab,
+                        destinations = navigationTabs,
+                        selected = currentDestination == navigationTab.direction,
+                        modifier = Modifier.fillMaxHeight()
+                    )
+                }
+            }
         }
+    }
+}
+
+@Suppress("MagicNumber")
+@Composable
+private fun ActiveTabIndication(
+    position: Int,
+    tabsCount: Int,
+    isVisible: Boolean
+) {
+    IconAnimatedVisibility(position != -1 && isVisible) {
+        val screenWidth = LocalConfiguration.current.screenWidthDp
+        val offsetValue = with(LocalDensity.current) {
+            val tabOffset = screenWidth / tabsCount / 2
+            val offsetDp = screenWidth / tabsCount * position + tabOffset - 12
+            offsetDp.dp.roundToPx()
+        }
+
+        val offset by animateIntOffsetAsState(
+            targetValue = IntOffset(offsetValue, 0),
+            animationSpec = tween(
+                durationMillis = 300,
+                easing = FastOutSlowInEasing
+            ),
+            label = "indication_offset"
+        )
+
+        Spacer(
+            Modifier
+                .offset { offset }
+                .width(24.dp)
+                .height(2.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(10.dp)
+                )
+        )
     }
 }
--- app/src/main/java/com/danilovfa/pexels/presentation/common/view/bottomnavigation/BottomNavigationTab.kt
+++ app/src/main/java/com/danilovfa/pexels/presentation/common/view/bottomnavigation/BottomNavigationTab.kt
@@ -1,14 +1,9 @@
 package com.danilovfa.pexels.presentation.common.view.bottomnavigation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
@@ -16,7 +11,6 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
@@ -31,19 +25,6 @@ internal fun RowScope.BottomNavigationTab(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier.weight(1f)) {
-        if (selected) {
-            Spacer(
-                Modifier
-                    .width(24.dp)
-                    .height(2.dp)
-                    .background(
-                        color = MaterialTheme.colorScheme.primary,
-                        shape = RoundedCornerShape(10.dp)
-                    )
-                    .align(Alignment.TopCenter)
-            )
-        }
-
         this@BottomNavigationTab.NavigationBarItem(
             icon = {
                 Box(modifier = Modifier.wrapContentWidth(), contentAlignment = Alignment.Center) {
